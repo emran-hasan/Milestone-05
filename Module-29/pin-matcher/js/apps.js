10acc745ff5f508f85434afd6835be397f67eb62
M Emran Hasan <emranhasan906@gmail.com>
2023-02-15 12:31:28 +0600

    new file added

--- Module-29/pin-matcher/js/apps.js
+++ Module-29/pin-matcher/js/apps.js
@@ -18,7 +18,60 @@ document.getElementById('generate-pin').addEventListener('click', function(){
     const pinNew =getPin();
 
     const displayPinField =  document.getElementById('display-pin');
-    displayPinField.value = pinNew;
-    
+    displayPinField.value = pinNew;  
 
+})
+
+// calculate pin 
+document.getElementById('calculator').addEventListener('click', function(event){
+    const number = event.target.innerText;
+    const typedField = document.getElementById('typed-field');
+    const typedFieldString = typedField.value;
+    
+    if(isNaN(number)){
+        
+        if(number === 'C'){
+            typedField.value = '';
+        }else if(number === '<'){
+            const digit = typedFieldString.split('');
+            digit.pop();
+            const remaining = digit.join('');
+            typedField.value =remaining;
+        }
+        
+    }else{
+        const newTypedNumber = typedFieldString + number;
+        typedField.value = newTypedNumber;
+    }
+})
+
+// submit button 
+document.getElementById('btn-notify').addEventListener('click', function(){
+    const displayPinField = document.getElementById('display-pin');
+    const currentPin = displayPinField.value;
+
+    const typedField = document.getElementById('typed-field');
+    const typed = typedField.value;
+
+    // if(typed === currentPin){
+        
+    //     const successPin = document.getElementById('pin-success')
+    //     successPin.style.display = 'block';
+
+    // }else{
+    //     const unsuccessPin = document.getElementById('pin-unsuccess');
+    //     unsuccessPin.style.display = 'block'
+
+    // }
+    const successPin = document.getElementById('pin-success')
+    const unsuccessPin = document.getElementById('pin-unsuccess');
+    if(typed === currentPin){
+        successPin.style.display = 'block';
+        unsuccessPin.style.display = 'none';
+
+    }else{
+        unsuccessPin.style.display = 'block';
+        successPin.style.display = 'none';
+
+    }
 })
